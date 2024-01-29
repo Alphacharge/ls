c4932c44ee99f040e90007fdcba9323fcb9f2e57
Alphacharge <rbetz@student.42heilbronn.de>
2024-01-29 10:38:46 +0100

    added libft submodule lib

---
+++ lib/libft/ft_atol.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbetz <rbetz@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/31 17:27:07 by rbetz             #+#    #+#             */
+/*   Updated: 2023/04/07 13:45:05 by rbetz            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+long	ft_atol(char *str)
+{
+	long			vz;
+	unsigned long	c;
+
+	vz = 1;
+	c = 0;
+	while (ft_isspace(*str))
+		str++;
+	if (*str == '-')
+	{
+		str++;
+		vz = -1;
+	}
+	else if (*str == '+')
+		str++;
+	if (!ft_strisdigit(str))
+		return (0);
+	while (ft_isdigit(*str))
+	{
+		c = c * 10 + *(str++) - '0';
+		if (c > INT64_MAX && vz == -1)
+			return (0);
+		else if (c > (INT64_MAX - 1) && vz == 1)
+			return (-1);
+	}
+	return ((long)(c * vz));
+}
