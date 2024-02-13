9fabfa0d00becf901ff0d890d573624525db9e19
Alphacharge <rbetz@student.42heilbronn.de>
2024-02-13 15:05:56 +0100

    added test reference

---
+++ gen_test.sh
@@ -0,0 +1,34 @@
+dir="tests"
+
+ls > $dir/ls
+ls -a > $dir/ls-a
+ls -l > $dir/ls-l
+ls -r > $dir/ls-r
+ls -R > $dir/ls-R
+ls -t > $dir/ls-t
+ls -al > $dir/ls-al
+ls -aR > $dir/ls-aR
+ls -ar > $dir/ls-ar
+ls -at > $dir/ls-at
+ls -lR > $dir/ls-lR
+ls -lr > $dir/ls-lr
+ls -lt > $dir/ls-lt
+ls -Rr > $dir/ls-Rr
+ls -Rt > $dir/ls-Rt
+ls -rt > $dir/ls-rt
+ls -alR > $dir/ls-alR
+ls -alr > $dir/ls-alr
+ls -alt > $dir/ls-alt
+ls -aRr > $dir/ls-aRr
+ls -aRt > $dir/ls-aRt
+ls -art > $dir/ls-art
+ls -lRr > $dir/ls-lRr
+ls -lRt > $dir/ls-lRt
+ls -lrt > $dir/ls-lrt
+ls -Rrt > $dir/ls-Rrt
+ls -alRr > $dir/ls-alRr
+ls -alRt > $dir/ls-alRt
+ls -alrt > $dir/ls-alrt
+ls -aRrt > $dir/ls-aRrt
+ls -lRrt > $dir/ls-lRrt
+ls -alRrt > $dir/ls-alRrt
