e66450749f95e6019714696aeed659e434b1ce49
Alphacharge <rbetz@student.42heilbronn.de>
2024-02-13 11:11:12 +0100

    reworked mergesort

--- src/sorting/mergesort.c
+++ src/sorting/mergesort.c
@@ -2,28 +2,47 @@
 
 t_file	*merge(t_file *left, t_file *right)
 {
-	if (left && !right)
-		return left;
-	else if (!left && right)
-		return right;
-	else if (!left && !right)
-		return NULL;
-	
-	char	*pair[3];
-	pair[0] = left->name;
-	pair[1] = right->name;
-	pair[2] = NULL;
-	bubblesort(pair, 2, F_ISSET(*(left->data->flags), F_REVERSE));
-	if (!ft_strcmp(pair[0], left->name)){
-		left->next = right;
-		left->listsize++;
-		return left;
-	} else {
-		right->next = left;
-		right->listsize++;
-		return right;
-	}
+	t_file	*new = NULL;
+	t_file	*tmp = NULL;
 
+	if (left && !right)
+		new = left;
+	else if (!left && right)
+		new = right;
+	else 
+	{
+		if (ft_strcmp(left->name, right->name) < 0)
+		{
+			new = left;
+			left = left->next;
+		}
+		else
+		{
+			new = right;
+			right = right->next;
+		}
+	}
+	tmp = new;
+	while (left && right)
+	{
+		if (ft_strcmp(left->name, right->name) < 0)
+		{
+			tmp->next = left;
+			left = left->next;
+		}
+		else
+		{
+			tmp->next = right;
+			right = right->next;
+		}
+		tmp = tmp->next;
+	}
+	if (left)
+		tmp->next = left;
+	else if (right)
+		tmp->next = right;
+	new->listsize = listsize(new);
+	return new;
 }
 
 t_file	*mergesortlist(t_file *tree)
@@ -32,33 +51,21 @@ t_file	*mergesortlist(t_file *tree)
 		return tree;
 	
 	unsigned int	midpoint = tree->listsize / 2;
-	unsigned int	counter = 0;
+	unsigned int 	i = 0;
 	t_file			*left = tree;
 	t_file			*right = tree;
-
-	while (right && right->next && counter < midpoint) {
+	t_file			*tmp = NULL;
+	while (right && i < midpoint) {
+		tmp = right;
 		right = right->next;
-		counter++;
+		i++;
 	}
-	ft_printf("lstsize:%d\tmidpoint:%d\tcounter:%d\n", tree->listsize, midpoint, counter);
-	ft_printf("tree:\t%p\nleft:\t%p\nright:\t%p\n", tree, left, right);
-	return NULL;
-	// unsigned int	midpoint = tree->listsize / 2;
-	// unsigned int 	i = 0;
-	// t_file			*left = tree;
-	// t_file			*right = tree;
-	// t_file			*tmp = NULL;
-	// for (; right && i < midpoint; i++) {
-	// 	right = right->next;
-	// }
-	// tmp = right;
-	// right = right->next;
-	// left->listsize = i;
-	// right->listsize = tree->listsize - i;
-	// tmp->next = NULL;
+	tmp->next = NULL;
+	left->listsize = listsize(left);
+	right->listsize = listsize(right);
 
-	// left = mergesortlist(left);
-	// right = mergesortlist(right);
+	left = mergesortlist(left);
+	right = mergesortlist(right);
 
-	// return merge(left, right);
+	return merge(left, right);
 }
