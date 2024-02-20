551a8f9e1690e6da152579d14ae88ef213d982b2
Alphacharge <rbetz@student.42heilbronn.de>
2024-02-20 09:36:52 +0100

    if time is equal, return sort by alpha instead 0

--- src/sorting/sort_by_mtime.c
+++ src/sorting/sort_by_mtime.c
@@ -4,7 +4,8 @@ long	sort_by_mtime(t_file *left, t_file *right)
 {
 	time_t	left_time = left->stat.st_mtimespec.tv_sec;
 	time_t	right_time = right->stat.st_mtimespec.tv_sec;
+	ft_printf("Left: %s,time: %d\tRight: %s,time:%d\n", left->name, left_time, right->name, right_time);
 	if (left_time != right_time)
 		return (F_ISSET(*(left)->data->flags, F_REVERSE) ? left_time < right_time : left_time > right_time);
-	return 0;
+	return SORT_BY_ALPHA(left, right);
 }
