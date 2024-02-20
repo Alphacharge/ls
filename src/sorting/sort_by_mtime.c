b66fd2c6af528742a5791d8a5649d3e21c0b6764
Alphacharge <rbetz@student.42heilbronn.de>
2024-02-20 15:28:23 +0100

    removed printing, added macro

--- src/sorting/sort_by_mtime.c
+++ src/sorting/sort_by_mtime.c
@@ -2,9 +2,8 @@
 
 long	sort_by_mtime(t_file *left, t_file *right)
 {
-	time_t	left_time = left->stat.st_mtimespec.tv_sec;
-	time_t	right_time = right->stat.st_mtimespec.tv_sec;
-	ft_printf("Left: %s,time: %d\tRight: %s,time:%d\n", left->name, left_time, right->name, right_time);
+	time_t	left_time = left->TIME;
+	time_t	right_time = right->TIME;
 	if (left_time != right_time)
 		return (F_ISSET(*(left)->data->flags, F_REVERSE) ? left_time < right_time : left_time > right_time);
 	return SORT_BY_ALPHA(left, right);
