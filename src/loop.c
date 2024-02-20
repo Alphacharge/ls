ee69339d0a704973c14ed5be4ce94fbaf011e11d
Alphacharge <rbetz@student.42heilbronn.de>
2024-02-20 15:58:07 +0100

    replaced generate_tree

---
+++ src/loop.c
@@ -0,0 +1,72 @@
+#include "ft_ls.h"
+
+void	loop(t_data *data, t_file *treelvl, char *path, DIR *ref)
+{
+	if (treelvl != NULL)
+	{
+// ft_printf("1\tlistsize:%d\n", treelvl->listsize);
+// print_inline_tree(treelvl);
+		treelvl = mergesortlist(treelvl);
+// print_inline_tree(treelvl);
+		print_treelvl(treelvl);
+		if (ref)
+			closedir(ref);
+		ft_free_tree(treelvl);
+	}
+	else
+	{
+		if (ref == NULL)
+			ft_error(data, 6);
+// ft_printf("2\n");
+		struct dirent *dir = readdir(ref);
+		t_file	*head = NULL;
+		while (dir != NULL) {
+// ft_printf("3\n");
+			t_file	*new = new_node(data);
+			if (new == NULL)
+				ft_error(data, 3);
+			if (head == NULL)
+				head = new;
+			else
+			{
+				t_file	*last = head;
+				while (last && last->next)
+					last = last->next;
+				last->next = new;
+			}
+			set_file_type(new, dir->d_type);
+			new->name = ft_strdup(dir->d_name);
+			new->path = path;
+			new->fullpath_name = ft_multijoin(false, 3, path, "/", dir->d_name);
+			new->length = NAMELENGTH;
+			if (new->length > head->maxlength)
+				update_maxlength(head, new->length);
+			if (new->name == NULL || new->fullpath_name == NULL)
+				ft_error(data, 5);
+			if ((F_ISSET(*new->data->flags, F_LONG) || F_ISSET(*new->data->flags, F_MTIME)) && lstat(new->fullpath_name, &new->stat) < 0)
+				ft_error(data, 4);
+			dir = readdir(ref);
+			head->listsize = listsize(head);
+		}
+		t_file *current = head;
+		if (current && !is_special_dir(current->name) && F_ISSET(*(data->flags), F_RECURSIVE) && current->type == _DIR)
+		{
+// ft_printf("4\n");
+			while (current) {
+// ft_printf("5\n");
+				DIR	*dir = opendir(current->fullpath_name);
+				if (dir == NULL) {
+					perror(current->fullpath_name);
+					break;
+				}
+				loop(data, current, current->fullpath_name, dir);
+			current = current->next;
+			}
+		}
+		else {
+// ft_printf("6\n");
+			loop(data, current, current->fullpath_name, NULL);
+		}
+		// ft_printf("lstsize: %d\n", listsize(head));
+	}
+}
