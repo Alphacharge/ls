d2e08b0204c9316ec513a65525696e454e310f65
Alphacharge <rbetz@student.42heilbronn.de>
2024-02-22 19:12:45 +0100

    removed redirection condition

--- src/printing/fillup_and_gap.c
+++ src/printing/fillup_and_gap.c
@@ -1,8 +1,7 @@
 #include "ft_ls.h"
 
-void	fillup_and_gap(unsigned short *mask, unsigned int length, unsigned int maxlength)
+void	fillup_and_gap(unsigned int length, unsigned int maxlength)
 {
-	if (F_ISSET(*mask, F_STDOUT)){
 	unsigned int spaces_needed = (TAB_WIDTH - (length % TAB_WIDTH)) % TAB_WIDTH;
 	for (unsigned int i = 0; i < spaces_needed; ++i) {
 		write(1, " ", 1);
@@ -14,7 +13,4 @@ void	fillup_and_gap(unsigned short *mask, unsigned int length, unsigned int maxl
 		length += TAB_WIDTH;
 	}
 	write(1, "\t", 1);
-	}
-	else
-		write(1, "\n", 1);
 }
