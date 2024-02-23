cefca0346e5c8b8e0fecd62405164d29dd7df888
Alphacharge <rbetz@student.42heilbronn.de>
2024-02-23 14:23:43 +0100

    removed unused stuff, removed extra newline for recursion

--- src/printing/print_treelvl.c
+++ src/printing/print_treelvl.c
@@ -1,8 +1,8 @@
 #include "ft_ls.h"
 
-void	print_treelvl(t_file *tree)
+void	print_treelvl(t_file **tree)
 {
-	t_file	*this = tree;
+	t_file	*this = *tree;
 
 	while (this) {
 		//print names
@@ -12,19 +12,11 @@ void	print_treelvl(t_file *tree)
 			ft_printf("%s", this->name);
 		//gaps 4 stdout
 		if (this->next && this->next->name && F_ISSET(*(this->data->flags), F_STDOUT))
-			fillup_and_gap(this->length, tree->maxlength);
+			fillup_and_gap(this->length, (*tree)->maxlength);
 		else if (this->next && this->next->name && !F_ISSET(*(this->data->flags), F_STDOUT))
 			(DEBUG) ? (write(1, "6\n", 2)) : (write(1, "\n", 1));
 		this = this->next;
-		// ft_printf("this:%p:%s\n", this, this->fullpath_name);
-		if (this == NULL){
+		if (this == NULL)
 			(DEBUG) ? (write(1, "7\n", 2)) : (write(1, "\n", 1));
-			// if (tree->data && F_ISSET(*(tree->data->flags), F_RECURSIVE))
-			// 	(DEBUG) ? (ft_printf("8\n")) : (ft_printf("\n"));
-		}
 	}
-		// if (!F_ISSET(*(tree->data->flags), F_STDOUT))
-			// write(1, "\n", 1);
-	// if (!F_ISSET(*(tree->data->flags), F_STDOUT))
-	// 	write(1, "8\n", 2);
 }
