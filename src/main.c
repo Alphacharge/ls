a0d05d439fc900199a1ac19ffa4c97db07f466a1
Alphacharge <rbetz@student.42heilbronn.de>
2024-02-23 14:15:11 +0100

    removed sleep and closedir, added perror

--- src/main.c
+++ src/main.c
@@ -24,21 +24,21 @@ int main(int argc, char** argv) {
 	F_SET(bitmask, F_FOLDERSORT);
 	data->folders = mergesortlist(data->folders);
 	F_CLEAR(bitmask, F_FOLDERSORT);
-	// bubblesort(data->folders, ft_arraycount(data->folders), F_ISSET(*(data->flags), F_REVERSE));
 
 	//generate, sort, print and free tree for every parameter
 	t_file	*current = data->folders;
 	while (current != NULL) {
 		DIR	*dir = opendir(current->fullpath_name);
-		if (dir == NULL)
+		if (dir == NULL){
+			perror(current->fullpath_name);
 			ft_error(data, 6);
+		}
 		if (!isSpecialDir(current->fullpath_name) && listSize(data->folders) > 1)
 			(DEBUG) ? (ft_printf("%s:0\n", current->fullpath_name)) : ft_printf("%s:\n", current->fullpath_name);
-		loop(data, NULL, current->fullpath_name, dir);
+		loop(&data, NULL, current->fullpath_name, dir);
 		if (!isSpecialDir(current->fullpath_name) && current->next != NULL)
 			(DEBUG) ? (write(1, "9\n", 2)) : (write(1, "\n", 1));
 		current = current->next;
-		closedir(dir);
 	}
 	ft_free_tree(data->folders);
 	ft_free((void**)&data);
