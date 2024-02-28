a9829469041eca9b34dc34da9ccc28bde5b66d34
Alphacharge <rbetz@student.42heilbronn.de>
2024-02-28 16:06:56 +0100

    reduced i/o calls on intends through const array

--- src/printing/printBytes.c
+++ src/printing/printBytes.c
@@ -2,12 +2,21 @@
 
 void	printBytes(t_file **this)
 {
-	unsigned int maxChars = countDigits((*this)->maxBytes);
-	unsigned int thisChars = countDigits((*this)->stat.st_size);
+	const char	*spaces[10] = {" ", "  ", "   ", "    ", "     ", "      ", "       ", "        ", "         ", "          "};
+	unsigned int	diff = countDigits((*this)->maxBytes) - countDigits((*this)->stat.st_size) + 1;
 
-	write(1, " ", 1);
-	while (thisChars++ < maxChars)
-		write(1, " ", 1);
+	while (diff > 10){
+		write(1, spaces[9], 10);
+		diff -= 10;
+	}
+	if (diff > 0)
+		write(1, spaces[diff - 1], diff);
+	// unsigned int maxChars = countDigits((*this)->maxBytes);
+	// unsigned int thisChars = countDigits((*this)->stat.st_size);
+
+	// write(1, " ", 1);
+	// while (thisChars++ < maxChars)
+	// 	write(1, " ", 1);
 	char *links = ft_ltoa((*this)->stat.st_size);
 	ft_putstr_fd(links, 1);
 	ft_free((void **)&links);
--- src/printing/printLinks.c
+++ src/printing/printLinks.c
@@ -2,14 +2,21 @@
 
 void	printLinks(t_file **this)
 {
-	unsigned int maxChars = countDigits((*this)->maxLinks);
-	unsigned int thisChars = countDigits((*this)->stat.st_nlink);
+	const char	*spaces[10] = {" ", "  ", "   ", "    ", "     ", "      ", "       ", "        ", "         ", "          "};
+	unsigned int	diff = countDigits((*this)->maxLinks) - countDigits((*this)->stat.st_nlink) + 1;
 
-	write(1, " ", 1);
-	// ft_printf("this:%d,max:%d\n", thisChars, maxChars);
-	while (thisChars++ < maxChars)
-		write(1, " ", 1);
-	// ft_printf("this:%d,max:%d\n", thisChars, maxChars);
+	while (diff > 10){
+		write(1, spaces[9], 10);
+		diff -= 10;
+	}
+	if (diff > 0)
+		write(1, spaces[diff - 1], diff);
+	// unsigned int maxChars = countDigits((*this)->maxLinks);
+	// unsigned int thisChars = countDigits((*this)->stat.st_nlink);
+
+	// write(1, " ", 1);
+	// while (thisChars++ < maxChars)
+	// 	write(1, " ", 1);
 	char *links = ft_ltoa((*this)->stat.st_nlink);
 	ft_putstr_fd(links, 1);
 	ft_free((void **)&links);
