fc6c97293dff6c7c7a340e63241c2993ccccbfeb
Alphacharge <rbetz@student.42heilbronn.de>
2024-02-29 11:33:30 +0100

    fixed missalligned filesize

--- src/printing/printValue.c
+++ src/printing/printValue.c
@@ -2,12 +2,23 @@
 
 void	printValue(t_file **this, unsigned int maxValue, short link)
 {
-	const char	*spaces[10] = {" ", "  ", "   ", "    ", "     ", "      ", "       ", "        ", "         ", "          "};
-	unsigned int	diff;
-	if (link)
+	const char		*spaces[10] = {" ", "  ", "   ", "    ", "     ", "      ", "       ", "        ", "         ", "          "};
+	unsigned int	diff = 0;
+	char			*value = NULL;
+
+	if (link) {
 		diff = countDigits(maxValue) - countDigits((*this)->stat.st_nlink) + 1;
+		value = ft_ltoa((*this)->stat.st_nlink);
+	}
 	else
+	{
 		diff = countDigits(maxValue) - countDigits((*this)->stat.st_size) + 1;
+		value = ft_ltoa((*this)->stat.st_size);
+	}
+	if (value == NULL){
+		ft_putstr_fd("ltoa malloc error\n", 2);
+		ft_error((*this)->data, 1);
+	}
 
 	while (diff > 10){
 		write(1, spaces[9], 10);
@@ -15,12 +26,8 @@ void	printValue(t_file **this, unsigned int maxValue, short link)
 	}
 	if (diff > 0)
 		write(1, spaces[diff - 1], diff);
-	char *links = ft_ltoa((*this)->stat.st_nlink);
-	if (links == NULL){
-		ft_putstr_fd("ltoa malloc error\n", 2);
-		ft_error((*this)->data, 1);
-	}
-	ft_putstr_fd(links, 1);
-	ft_free((void **)&links);
+
+	ft_putstr_fd(value, 1);
+	ft_free((void **)&value);
 	write(1, " ", 1);
 }
