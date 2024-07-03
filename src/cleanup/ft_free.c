1d8f46b65eb4162e8bdaeac48b85a4da6ec3113f
Alphacharge <rbetz@student.42heilbronn.de>
2024-07-03 19:15:18 +0200

    fixed messed up freetree

--- src/cleanup/ft_free.c
+++ src/cleanup/ft_free.c
@@ -1,5 +1,10 @@
 #include "ft_ls.h"
 
+/**
+ * Frees the memory pointed to by the pointer **tofree and sets it to NULL to prevent
+ * dangling pointers.
+ * @param tofree - Double pointer to dynamically allocated memory.
+ */
 void	ft_free(void **tofree)
 {
 	free(*tofree);
--- src/cleanup/ft_free_tree.c
+++ src/cleanup/ft_free_tree.c
@@ -1,12 +1,20 @@
 #include "ft_ls.h"
 
 /**
- * Frees the memory pointed to by the pointer **tofree and sets it to NULL to prevent
- * dangling pointers.
- * @param tofree - Double pointer to dynamically allocated memory.
+ * Recursively frees the memory allocated for a linked list of t_file structures,
+ * including their associated strings (fullPathName, fileName, userName, groupName).
+ * @param tree - Pointer to the head of the linked list of t_file structures to be freed.
  */
-void	ft_free(void **tofree)
+void	ft_free_tree(t_file	*tree)
 {
-	free(*tofree);
-	*tofree = NULL;
+	t_file	*tmp;
+	while (tree) {
+		tmp = tree;
+		ft_free((void**)&tree->fullPathName);
+		ft_free((void**)&(tree->fileName));
+		ft_free((void**)&(tree->userName));
+		ft_free((void**)&(tree->groupName));
+		tree = tree->next;
+		ft_free((void**)&(tmp));
+	}
 }
